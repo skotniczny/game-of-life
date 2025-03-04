8745cf6945d314b13c6f501ad4845233fdf1c420
Łukasz <skotniczny@outlook.com>
2025-03-04 18:56:03 +0100

    Refactor app logic to use ES6 modules

--- game-of-life-canvas.js
+++ game-of-life-canvas.js
@@ -1,142 +1,13 @@
-var canvas = $("board");
+import Board from "./modules/board.mjs";
+import { qS as $ } from "./modules/dom-utils.mjs";
 
-/* Make array of booleans */
-function makeGrid(checkboxes) {
-  return checkboxes.map(function () {
-    return Math.random() < 0.25;
-  });
-}
+const canvas = $("board");
 
-function Block(ctx, col, row, size) {
-  this.col = col;
-  this.row = row;
-  this.blockSize = size;
-  this.ctx = ctx;
-}
-
-Block.prototype.draw = function (color) {
-  var x = this.col * this.blockSize;
-  var y = this.row * this.blockSize;
-  this.ctx.fillStyle = color;
-  this.ctx.fillRect(x + 1, y + 1, this.blockSize - 1, this.blockSize - 1);
-};
-
-function Board(element) {
-  this.blockSize = 8;
-  this.gridWidth = Math.floor(element.width / this.blockSize);
-  this.gridHeight = Math.floor(element.height / this.blockSize);
-
-  /* Create board */
-  this.arrayOfBlocks = makeBoard(element, this.gridWidth, this.gridHeight, this.blockSize);
-  this.grid = makeGrid(this.arrayOfBlocks);
-
-  /* Loop and Timer */
-  this.boardSpeed = 50;
-  this.stop = false;
-
-  /* Make array of Block objects */
-  function makeBoard(element, width, height, blockSize) {
-    var blocks = [];
-    var size = width * height;
-
-    for (var i = 0; i < size; i += 1) {
-      var col = i % width;
-      var row = (i - col) / width;
-      blocks.push(new Block(element.getContext("2d"), col, row, blockSize));
-    }
-    return blocks;
-  }
-}
-
-/* Calculate next generation */
-Board.prototype.countNeighbours = function (x, y) {
-  var directions = [{ x: 0, y: -1 }, { x: 1, y: -1 }, { x: 1, y: 0 }, { x: 1, y: 1 }, { x: 0, y: 1 }, { x: -1, y: 1 }, { x: -1, y: 0 }, { x: -1, y: -1 }];
-  var neighbours = 0;
-  for (var i = 0; i < 8; i++) {
-    var dir = directions[i];
-    var dirX = x + dir.x;
-    var dirY = y + dir.y;
-    if (dirX >= 0 && dirX < this.gridWidth && dirY >= 0 && dirY < this.gridHeight) {
-      var index = dirX + (dirY * this.gridWidth);
-      neighbours += this.grid[index] ? 1 : 0;
-    }
-  }
-  return neighbours;
-}
-
-Board.prototype.nextGeneration = function () {
-  var that = this;
-  this.grid = this.grid.map(function (item, index) {
-    var x = index % that.gridWidth;
-    var y = (index - x) / that.gridWidth;
-    var neighbours = that.countNeighbours(x, y);
-    if (neighbours < 2 || neighbours > 3) {
-      return false;
-    }
-    if (neighbours === 2) {
-      return item;
-    }
-    return true;
-  });
-}
-
-/* Update board */
-Board.prototype.updateBoard = function () {
-  var that = this;
-  this.arrayOfBlocks.forEach(function (checkbox, index) {
-    var color = (that.grid[index]) ? "#335B5C" : "#ffffff";
-    checkbox.draw(color);
-  });
-}
-
-Board.prototype.newGrid = function () {
-  this.grid = makeGrid(this.arrayOfBlocks);
-  this.updateBoard();
-}
-
-Board.prototype.clearBoard = function () {
-  this.grid = this.grid.map(function () {
-    return false;
-  });
-  this.updateBoard();
-  this.pause()
-}
-
-Board.prototype.checkBlock = function (index) {
-  this.grid[index] = !this.grid[index];
-  this.updateBoard();
-}
-
-Board.prototype.step = function () {
-  this.nextGeneration();
-  this.updateBoard();
-}
-
-Board.prototype.play = function () {
-  this.stop = false;
-  var that = this;
-  function loop() {
-    that.step();
-    if (!that.stop) {
-      setTimeout(loop, that.boardSpeed);
-    }
-  }
-  loop()
-}
-
-Board.prototype.pause = function () {
-  this.stop = true;
-}
-
-Board.prototype.speed = function (value) {
-  this.boardSpeed = value;
-}
-
-var board = new Board(canvas);
+const board = new Board(canvas);
 board.play();
 
 /* Play Button */
-var playBtn = $("play");
+const playBtn = $("play");
 playBtn.addEventListener("click", function () {
   if (board.stop) {
     this.textContent = "Stop";
@@ -148,13 +19,13 @@ playBtn.addEventListener("click", function () {
 });
 
 /* Next Button */
-var nextBtn = $("next");
+const nextBtn = $("next");
 nextBtn.addEventListener("click", function () {
   board.step();
 });
 
 /* New Button */
-var newBtn = $("new");
+const newBtn = $("new");
 newBtn.addEventListener("click", function () {
   board.newGrid();
   nextBtn.focus();
@@ -168,7 +39,7 @@ $("clear").addEventListener("click", function () {
 });
 
 /* Speed control, min - 2 fps, max - 20 fps */
-var slider = $("speed");
+const slider = $("speed");
 function logSlider(position) {
   var minp = slider.min;
   var maxp = slider.max;
@@ -186,15 +57,10 @@ slider.addEventListener("change", function () {
 });
 
 canvas.addEventListener("click", function (event) {
-  var rect = canvas.getBoundingClientRect();
-  var x = Math.floor((event.clientX - rect.left) / board.blockSize);
-  var y = Math.floor((event.clientY - rect.top) / board.blockSize);
-  var index = x + y * board.gridWidth;
+  const rect = canvas.getBoundingClientRect();
+  const x = Math.floor((event.clientX - rect.left) / board.blockSize);
+  const y = Math.floor((event.clientY - rect.top) / board.blockSize);
+  const index = x + y * board.gridWidth;
   board.checkBlock(index);
   nextBtn.focus();
 });
-
-/* Helper */
-function $(id) {
-  return document.getElementById(id);
-}
